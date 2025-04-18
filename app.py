cb7d290e8310e9fea5f653b8fa7d2d01b9cfdaea
Laiba1727 <laibawaseem475@gmail.com>
2025-04-18 22:49:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -161,4 +161,4 @@ def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=7860)
+    uvicorn.run("app:app", host="0.0.0.0", port=7860)
